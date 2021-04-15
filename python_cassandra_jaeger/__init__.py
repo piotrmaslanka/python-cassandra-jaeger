446069987e60cf163e105ca55e53b2d0f5d6a0eb
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-04-16 00:47:59 +0200

    v0.1

--- python_cassandra_jaeger/__init__.py
+++ python_cassandra_jaeger/__init__.py
@@ -6,7 +6,7 @@ from satella.cassandra import wrap_future
 from satella.coding.structures import Proxy
 from satella.opentracing import trace_future
 
-__version__ = '0.1a9'
+__version__ = '0.1'
 
 
 def _query_to_string(query, arguments):
